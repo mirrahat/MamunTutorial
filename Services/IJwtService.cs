bdb147e9a8b8e7e987cbf810b685d88ed39d08df
mirrahat <mirrahatfiverr@gmail.com>
2024-12-17 09:02:22 +0600

    next js connected CORS

--- Services/IJwtService.cs
+++ Services/IJwtService.cs
@@ -9,15 +9,16 @@ namespace MamunTutorial.Services
     public class IjwtService
     {
         private readonly IConfiguration _configuration;
+        private DateTime expiration;
 
-        public JwtService(IConfiguration configuration) {
+        /*public JwtService(IConfiguration configuration) {
         
             _configuration = configuration;
         
-        }
-        AuthenticationResponse CreateJwtToken(Applicationuser user) {
+        }*/
+        /*AuthenticationResponse CreateJwtToken(Applicationuser user) {
 
-            DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATION_MINUTES"]));
+         DateTime expiration=   DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATION_MINUTES"]));
             Claim[] claims = new Claim[] {
             new Claim(JwtRegisteredClaimNames.Sub,user.Id.Tostring()),
 
@@ -31,12 +32,11 @@ namespace MamunTutorial.Services
             };
 
             SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-            SigningCredentials signingCredential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             JwtSecurityToken tokeGenerator = new JwtSecurityToken(
 
                 _configuration["Jwt:Isuuer"],
-
                 _configuration["Jwt:Isuuer"],
                 claims,
                  expires: expiration,
@@ -44,10 +44,14 @@ namespace MamunTutorial.Services
                 );
 
             JwtSecurityTokenHandler tokenHandler= new JwtSecurityTokenHandler();
-            tokenHandler.WriteToken(tokenHandler);
+          
             string token = tokenHandler.WriteToken(tokeGenerator);
 
-        }
+            return new AuthenticationResponse() { Token = token, Email = user.Email,
+                PersonName = user.Name, Expiration = expiration
+            };
+
+        }*/
 
     }
 }
