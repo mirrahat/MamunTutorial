a35b88536c3c156fbe446e2fb243e3f28445f77b
mirrahat <mirrahatfiverr@gmail.com>
2025-02-05 10:07:02 +0600

    port updated

--- Program.cs
+++ Program.cs
@@ -100,6 +100,11 @@ builder.Services.AddAuthorization(options => { });
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddTransient<IJwtService, JwtService>();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
 
 var app = builder.Build();
 
