0dff48af6aace892eb548dfe50c0efe7a3690f04
mirrahat <mirrahatfiverr@gmail.com>
2025-02-06 06:10:29 +0600

    Added index.html to serve static content

--- obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35b88536c3c156fbe446e2fb243e3f28445f77b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275834b667780fed5476eb8964051d00a775c050")]
 [assembly: System.Reflection.AssemblyProductAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
